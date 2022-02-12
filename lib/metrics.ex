15c0fbbed01919ea5864a74e485915348824eeac
Miguel Coba <miguel.coba@gmail.com>
2022-02-12 11:19:08 +0100

    first commit

---
+++ lib/metrics.ex
@@ -0,0 +1,18 @@
+defmodule Metrics do
+  @moduledoc """
+  Documentation for `Metrics`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Metrics.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
